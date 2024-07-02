89d68da5f53cb2d24f7826e4b9d8e19434c56676
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-03 02:24:44 +0300

    using decorator for localeselector

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -11,18 +11,18 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# @babel.localeselector
+app = Flask(__name__)
+# uncomment the following line and @babel.localeselector for checker
+babel = Babel(app)
+# babel = Babel(app, locale_selector=get_locale)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@babel.localeselector
 def get_locale() -> str:
     """ Get locale from request """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-app = Flask(__name__)
-# uncomment the following line and @babel.localeselector for checker
-# babel = Babel(app)
-babel = Babel(app, locale_selector=get_locale)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
 
 
 @app.route('/', strict_slashes=False)
